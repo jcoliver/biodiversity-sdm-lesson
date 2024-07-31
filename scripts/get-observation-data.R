40f4a7326b6a0878dca169c4c13d9e5b95667ada
Jeff Oliver <jcoliver@email.arizona.edu>
2024-07-31 16:58:25 -0700

    Update header to iNaturalist download script

--- scripts/get-observation-data.R
+++ scripts/get-observation-data.R
@@ -1,68 +1,68 @@
 # Script to download file of observations from iNaturalist based on taxon_id
 # Jeff Oliver
-# jcoliver@email.arizona.edu
-# 2017-08-24
+# jcoliver@arizona.edu
+# 2024-07-31
 
 ################################################################################
 args = commandArgs(trailingOnly = TRUE)
-usage.string <- "Usage: Rscript --vanilla get-observation-data.R <taxon_id>"
+usage_string <- "Usage: Rscript --vanilla scripts/get-observation-data.R <taxon_id>"
 
 if (length(args) < 1) {
   stop(paste("get-observation-data requires a numerical taxon id", 
-             usage.string,
+             usage_string,
              sep = "\n"))
 }
 
-inat.taxon.id <- args[1]
-if (is.na(suppressWarnings(as.numeric(inat.taxon.id)))) {
+inat_taxon_id <- args[1]
+if (is.na(suppressWarnings(as.numeric(inat_taxon_id)))) {
   stop(paste0("get-observation-data requires a numerical taxon id\n",
-              "\"", inat.taxon.id, "\" is not a valid id\n",
-              usage.string,
+              "\"", inat_taxon_id, "\" is not a valid id\n",
+              usage_string,
               "\n"))
 }
 
-page.num <- 1
+page_num <- 1
 finished <- FALSE
-obs.data <- NULL
+obs_data <- NULL
 # Retrieving information from iNaturalist API
 # Don't know a priori how many pages of records there will be, so for now we'll
 # just keep doing GET requests, incrementing the `page` key until we get a 
 # result with zero observations (up to 99 requests, more than that requires 
 # authentication)
-while (!finished & page.num < 100) {
-  obs.url <- paste0("http://inaturalist.org/observations.csv?&taxon_id=", 
-                    inat.taxon.id, 
+while (!finished & page_num < 100) {
+  obs_url <- paste0("http://inaturalist.org/observations.csv?&taxon_id=", 
+                    inat_taxon_id, 
                     "&page=", 
-                    page.num,
+                    page_num,
                     "&quality_grade=research&has[]=geo")
-  temp.data <- read.csv(file = obs.url, stringsAsFactors = FALSE)
-  if (nrow(temp.data) > 0) {
-    if (is.null(obs.data)) {
-      obs.data <- temp.data
+  temp_data <- read.csv(file = obs_url, stringsAsFactors = FALSE)
+  if (nrow(temp_data) > 0) {
+    if (is.null(obs_data)) {
+      obs_data <- temp_data
     } else {
-      obs.data <- rbind(obs.data, temp.data)
+      obs_data <- rbind(obs_data, temp_data)
     }
   } else {
     finished <- TRUE
   }
-  page.num <- page.num + 1
-  rm(temp.data)
+  page_num <- page_num + 1
+  rm(temp_data)
 }
 
 # As long as there are records, write them to file
-if (nrow(obs.data) > 0) {
+if (nrow(obs_data) > 0) {
   if (!dir.exists("data")) {
     dir.create("data")
   }
   if (!dir.exists("data/inaturalist")) {
     dir.create("data/inaturalist")
   }
-  outfile <- paste0("data/inaturalist/", inat.taxon.id, "-iNaturalist.txt")
-  write.csv(x = obs.data, 
+  outfile <- paste0("data/inaturalist/", inat_taxon_id, "-iNaturalist.txt")
+  write.csv(x = obs_data, 
             file = outfile,
             row.names = FALSE,
             quote = TRUE) # Gotta quote strings, as they are likely to contain common seps (i.e. "," and "\t")
-  cat(paste0(nrow(obs.data), " records for taxon id ", inat.taxon.id, " written to ", outfile, "\n"))
+  cat(paste0(nrow(obs_data), " records for taxon id ", inat_taxon_id, " written to ", outfile, "\n"))
 } else {
-  cat(paste0("No records returned for taxon id = ", inat.taxon.id, "\n"))
+  cat(paste0("No records returned for taxon id = ", inat_taxon_id, "\n"))
 }
